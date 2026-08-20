7e43422f4c72123c573347ad7c66426d0959cf19
avosa <avosa.developer@gmail.com>
2026-08-20 04:13:51 -0700

    physics: cloth soft bodies and vertex readback

--- adapters/physics/physics_stub.zig
+++ adapters/physics/physics_stub.zig
@@ -63,6 +63,23 @@ pub const World = struct {
         _ = dt_seconds;
     }
 
+    pub fn addCloth(world: World, cols: u32, rows: u32, width: f32, height: f32, position: [3]f32) !u32 {
+        _ = world;
+        _ = cols;
+        _ = rows;
+        _ = width;
+        _ = height;
+        _ = position;
+        return error.BodyAddFailed;
+    }
+
+    pub fn clothRead(world: World, body: u32, out: []f32) u32 {
+        _ = world;
+        _ = body;
+        _ = out;
+        return 0;
+    }
+
     pub fn bodyPose(world: World, body: u32) ![16]f32 {
         _ = world;
         _ = body;
